df33160c74b7d81ed62de380073936332059d779
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2024-10-15 23:01:54 +0630

    fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} flex flex-col min-h-screen justify-between`}>
+        <body className={`${inter.className} flex flex-col min-h-screen justify-between bg-background text-foreground`}>
           <ThemeProvider
             attribute='class'
             defaultTheme='dark'
